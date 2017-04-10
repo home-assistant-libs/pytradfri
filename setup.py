c1785d07439b69dfa7799f2580cb5c3b1a996886
Patrik <slasktratt@gammelgravlingen.com>
2017-04-10 20:48:20 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+  name = 'openikeatradfri',
+  packages = ['openikeatradfri'],
+  version = '0.1',
+  description = 'A python library for communicating with the IKEA Tradfri (Trådfri) Gateway',
+  author = 'Patrik Lindgren',
+  author_email = 'slasktratt@gammelgravlingen.com',
+  url = 'https://github.com/ggravlingen/python-openikeatradfri',
+  download_url = 'https://github.com/ggravlingen/ikeatradfri/tarball/0.1'
+)
