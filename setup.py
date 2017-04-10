b4bae64303441d7b342a14f952844d037b556e3f
Patrik <slasktratt@gammelgravlingen.com>
2017-04-10 21:16:31 +0200

    Updated readme

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
   packages = ['openikeatradfri'],
   version = '0.1',
   description = 'A python library for communicating with the IKEA Tradfri (Trådfri) Gateway',
-  author = 'Patrik Lindgren',
+  author = 'Patrik L',
   author_email = 'slasktratt@gammelgravlingen.com',
   url = 'https://github.com/ggravlingen/python-openikeatradfri',
-  download_url = 'https://github.com/ggravlingen/ikeatradfri/tarball/0.1'
+  download_url = 'https://github.com/ggravlingen/python-openikeatradfri/archive/v1.tar.gz'
 )
