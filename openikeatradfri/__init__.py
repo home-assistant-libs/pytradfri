db97aeaa917b996a3223022cc8a77af86ed268ed
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-04-10 23:34:27 -0700

    Export some objects in __init__.py

--- openikeatradfri/__init__.py
+++ openikeatradfri/__init__.py
@@ -1,1 +1,8 @@
 """Implement an API wrapper around Ikea Tradfri."""
+
+from .coap_cli import api_factory as cli_api_factory
+from .error import PyTradFriError, CommandError, NotFoundError
+from .gateway import Gateway
+
+__all__ = ['Gateway', 'cli_api_factory', 'PyTradFriError', 'CommandError',
+           'NotFoundError']
