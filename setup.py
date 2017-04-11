116488c8ca6018002d14289dca98b2322b7ef4ae
Patrik <slasktratt@gammelgravlingen.com>
2017-04-11 16:15:03 +0200

    Added Docker support for Raspberry Pi 3 and bumped version for PyPi

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 setup(
   name = 'openikeatradfri',
   packages = ['openikeatradfri'],
-  version = '0.1',
-  description = 'A python library for communicating with the IKEA Tradfri (Trådfri) Gateway',
+  version = '0.3',
+  description = 'A python library for communicating with the IKEA Tradfri Gateway',
   author = 'Patrik L',
   author_email = 'slasktratt@gammelgravlingen.com',
   url = 'https://github.com/ggravlingen/python-openikeatradfri',
-  download_url = 'https://github.com/ggravlingen/python-openikeatradfri/archive/v1.tar.gz'
+  download_url = 'https://github.com/ggravlingen/python-openikeatradfri/archive/v0.2.tar.gz'
 )
