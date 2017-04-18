268b45a6d61dc69c5c78708af8da4289383e6620
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2017-04-18 07:51:15 -0700

    Version bump to 1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
+
+VERSION = 1.0
+DOWNLOAD_URL = \
+    'https://github.com/ggravlingen/pytradfri/archive/{}.zip'.format(VERSION)
+
 setup(
-  name = 'pytradfri',
-  packages = ['pytradfri'],
-  version = '0.4',
-  description = 'A python library for communicating with the Tradfri Gateway',
-  author = 'balloob, ggravlingen',
-  author_email = 'no@email.com',
-  url = 'https://github.com/ggravlingen/pytradfri',
-  download_url = 'https://github.com/ggravlingen/pytradfri/archive/v0.4.zip'
+  name='pytradfri',
+  packages=['pytradfri'],
+  version=VERSION,
+  description='A python library for communicating with the Tradfri Gateway',
+  author='balloob, ggravlingen',
+  author_email='no@email.com',
+  url='https://github.com/ggravlingen/pytradfri',
+  download_url=DOWNLOAD_URL
 )
