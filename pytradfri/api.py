67f2f7df339609b1d8ec94bff7b2b11a3ff6ab8f
Lewis Juggins <ldjuggins@gmail.com>
2017-05-31 07:47:42 +0100

    Correctly wrap retry func (#33)

--- pytradfri/api.py
+++ pytradfri/api.py
@@ -1,10 +1,12 @@
 """API utilities."""
+from functools import wraps
 
 from .error import RequestTimeout
 
 
 def retry_timeout(api, retries=3):
     """Retry API call when a timeout occurs."""
+    @wraps(api)
     def retry_api(*args, **kwargs):
         """Retrying API."""
         for i in range(1, retries + 1):
