4b0fe7829e0d4bc8daf657389571a28a314e99ae
Lennart Buhl <git@hackmate.de>
2017-08-23 15:23:06 +0200

    add conversion from Kelvin to xyY coordinates

--- pytradfri/color.py
+++ pytradfri/color.py
@@ -49,6 +49,29 @@ def x_to_kelvin(x):
     return int(offset * higher + (1 - offset) * lower)
 
 
+def kelvin_to_xyY(T):
+    # Sources: "Design of Advanced Color - Temperature Control System for HDTV Applications" [Lee, Cho, Kim]
+    # and https://en.wikipedia.org/wiki/Planckian_locus#Approximation
+    # and http://fcam.garage.maemo.org/apiDocs/_color_8cpp_source.html
+    if not (1667 <= T <= 25000):
+        return None, None
+
+    if T <= 4000:
+        # One number differs on Wikipedia and the paper: 0.2343589 is 0.2343580 on Wikipedia... don't know why
+        x = -0.2661239*(10**9)/T**3 - 0.2343589*(10**6)/T**2 + 0.8776956*(10**3)/T + 0.17991
+    elif T <= 25000:
+        x = -3.0258469*(10**9)/T**3 + 2.1070379*(10**6)/T**2 + 0.2226347*(10**3)/T + 0.24039
+
+    if T <= 2222:
+        y = -1.1063814*x**3 - 1.3481102*x**2 + 2.18555832*x - 0.20219683
+    elif T <= 4000:
+        y = -0.9549476*x**3 - 1.37418593*x**2 + 2.09137015*x - 0.16748867
+    elif T <= 25000:
+        y = 3.081758*x**3 - 5.8733867*x**2 + 3.75112997*x - 0.37001483
+
+    return int(x*65535+0.5), int(y*65535+0.5)
+
+
 def rgb_to_xyY(r, g, b):
     # According to http://www.brucelindbloom.com/index.html?Eqn_RGB_to_XYZ.html
     # and http://www.brucelindbloom.com/index.html?Eqn_XYZ_to_xyY.html
