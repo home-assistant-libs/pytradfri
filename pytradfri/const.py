b9200194174a53e7efb205f5691278aa464ebdfa
paul <31916694+spektren@users.noreply.github.com>
2017-10-13 17:22:38 +0200

    Update const.py
    travisci

--- pytradfri/const.py
+++ pytradfri/const.py
@@ -21,8 +21,8 @@ ATTR_CURRENT_TIME_ISO8601 = "9060"
 ATTR_FIRST_SETUP = "9069"  # ??? unix epoch value when gateway first setup
 ATTR_GATEWAY_INFO = "15012"
 ATTR_GATEWAY_ID = "9081"  # ??? id of the gateway
-ATTR_GATEWAY_REBOOT = "9030"  # gw reboot 
-ATTR_GATEWAY_FACTORY_DEFAULTS = "9031"  # gw to factory defaults 
+ATTR_GATEWAY_REBOOT = "9030"  # gw reboot
+ATTR_GATEWAY_FACTORY_DEFAULTS = "9031"  # gw to factory defaults
 
 ATTR_LIGHT_STATE = "5850"  # 0 / 1
 ATTR_LIGHT_DIMMER = "5851"  # Dimmer, not following spec: 0..255
