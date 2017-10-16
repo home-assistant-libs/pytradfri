5470a2db5b121c4b65b5a866193aa4d9d41b81dc
rudyvan <rudy_vandenberghe@hotmail.com>
2017-10-16 11:51:52 +0200

    fixed formatting

--- pytradfri/device.py
+++ pytradfri/device.py
@@ -49,7 +49,7 @@ class Device(ApiResource):
     @property
     def has_light_control(self):
         return (self.raw is not None and 
-                len(self.raw.get(ATTR_LIGHT_CONTROL,"")) > 0)
+                len(self.raw.get(ATTR_LIGHT_CONTROL, "")) > 0)
 
     @property
     def light_control(self):
