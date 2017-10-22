3317279226536cdb6f3cf0ef2b5ee76503b40f5a
rudyvan <rudy_vandenberghe@hotmail.com>
2017-10-22 09:16:52 +0200

    Update device.py (#68)
    * Update device.py
    If the device is an input instead of a lamp then self.raw is None and has_light_control should return False. This change caters for that.

--- pytradfri/device.py
+++ pytradfri/device.py
@@ -49,8 +49,8 @@ class Device(ApiResource):
 
     @property
     def has_light_control(self):
-        return (ATTR_LIGHT_CONTROL in self.raw and
-                len(self.raw.get(ATTR_LIGHT_CONTROL)) > 0)
+        return (self.raw is not None and
+                len(self.raw.get(ATTR_LIGHT_CONTROL, "")) > 0)
 
     @property
     def light_control(self):
