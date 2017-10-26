044c895dd969c5074b12ff02a1a1d08530780330
Patrik <slasktratt@gammelgravlingen.com>
2017-10-26 19:41:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "3.0.2"
+VERSION = "3.0.3"
 DOWNLOAD_URL = \
     'https://github.com/ggravlingen/pytradfri/archive/{}.zip'.format(VERSION)
 
