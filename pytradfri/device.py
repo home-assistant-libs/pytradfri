b63ebf4bc49315ee1a6b911f412b548ecec06b0b
Patrik <slasktratt@gammelgravlingen.com>
2017-10-28 09:50:53 +0200

    Include pytradfrierror in device

--- pytradfri/device.py
+++ pytradfri/device.py
@@ -20,6 +20,7 @@ from .color import can_kelvin_to_xy, kelvin_to_xyY, xyY_to_kelvin, rgb_to_xyY,\
     xy_brightness_to_rgb, COLORS, MIN_KELVIN, MAX_KELVIN,\
     MIN_KELVIN_WS, MAX_KELVIN_WS
 from .resource import ApiResource
+from .error import PyTradFriError
 
 
 class Device(ApiResource):
