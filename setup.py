95ff66409246ac25037a8da2759adc7337f3bfc7
Patrik <slasktratt@gammelgravlingen.com>
2017-10-28 18:39:35 +0200

    Bump version to prepare for new release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "3.0.3"
+VERSION = "3.0.4"
 DOWNLOAD_URL = \
     'https://github.com/ggravlingen/pytradfri/archive/{}.zip'.format(VERSION)
 
