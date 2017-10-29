4b43367cc49696aa7fe7bc4efce03f1491c7eb39
Patrik <slasktratt@gammelgravlingen.com>
2017-10-29 09:19:05 +0100

    Removed dashes

--- script/install-aiocoap.sh
+++ script/install-aiocoap.sh
@@ -1,7 +1,7 @@
--#!/bin/sh
--cd ../..
--git clone https://github.com/chrysn/aiocoap
--cd aiocoap
--git reset --hard 3286f48f0b949901c8b5c04c0719dc54ab63d431
--python3 -m pip install --upgrade pip setuptools
--python3 -m pip install .
+#!/bin/sh
+cd ../..
+git clone https://github.com/chrysn/aiocoap
+cd aiocoap
+git reset --hard 3286f48f0b949901c8b5c04c0719dc54ab63d431
+python3 -m pip install --upgrade pip setuptools
+python3 -m pip install .
