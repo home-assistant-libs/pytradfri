94a811c8981b68607a5404f7a6a4047a0d7e8532
Patrik <slasktratt@gammelgravlingen.com>
2017-10-29 09:57:57 +0100

    Update device.py

--- pytradfri/device.py
+++ pytradfri/device.py
@@ -220,7 +220,7 @@ class LightControl:
             color = COLORS[colorname.lower().replace(" ", "_")]
             return self.set_hex_color(color, index=index)
         except KeyError:
-            PyTradFriError("Could not match color name in COLOR_NAMES")
+            raise PyTradFriError("Could not match color name in COLOR_NAMES")
 
     def set_rgb_color(self, r, g, b, *, index=0):
         return self.set_values(rgb_to_xyY(r, g, b), index=index)
