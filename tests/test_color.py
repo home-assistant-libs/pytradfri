a6ddd83c08b7a7d2794fa25fbab39383a8b20583
Patrik <slasktratt@gammelgravlingen.com>
2017-11-07 23:31:44 +0100

    Improved tests

--- tests/test_color.py
+++ tests/test_color.py
@@ -6,7 +6,7 @@ from pytradfri.const import (
     SUPPORT_COLOR_TEMP,
     SUPPORT_HEX_COLOR,
     SUPPORT_RGB_COLOR,
-    #  SUPPORT_XY_COLOR
+    SUPPORT_XY_COLOR
     )
 from pytradfri.color import can_kelvin_to_xy, kelvin_to_xyY, xyY_to_kelvin, \
     rgb_to_xyY, rgb2xyzD65, xy_brightness_to_rgb, light_supported_features
@@ -134,14 +134,20 @@ def test_xy_brightness_to_rgb():
 
 
 def test_supported_colors():
-    print(LIGHT_W[ATTR_LIGHT_CONTROL][0])
     assert light_supported_features(LIGHT_W[ATTR_LIGHT_CONTROL][0]) ==\
         SUPPORT_BRIGHTNESS
+
     assert light_supported_features(LIGHT_WS['3311'][0]) == SUPPORT_BRIGHTNESS\
-        + SUPPORT_COLOR_TEMP + SUPPORT_HEX_COLOR
+        + SUPPORT_COLOR_TEMP + SUPPORT_HEX_COLOR + SUPPORT_XY_COLOR
+
     assert light_supported_features(LIGHT_WS_CUSTOM_COLOR['3311'][0]) ==\
-        SUPPORT_BRIGHTNESS + SUPPORT_COLOR_TEMP + SUPPORT_HEX_COLOR
+        SUPPORT_BRIGHTNESS + SUPPORT_COLOR_TEMP + SUPPORT_HEX_COLOR + \
+        SUPPORT_XY_COLOR
+
     assert light_supported_features(LIGHT_CWS['3311'][0]) ==\
-        SUPPORT_BRIGHTNESS + SUPPORT_RGB_COLOR + SUPPORT_HEX_COLOR
+        SUPPORT_BRIGHTNESS + SUPPORT_RGB_COLOR + SUPPORT_HEX_COLOR + \
+        SUPPORT_XY_COLOR
+
     assert light_supported_features(LIGHT_CWS_CUSTOM_COLOR['3311'][0]) ==\
-        SUPPORT_BRIGHTNESS + SUPPORT_RGB_COLOR + SUPPORT_HEX_COLOR
+        SUPPORT_BRIGHTNESS + SUPPORT_RGB_COLOR + SUPPORT_HEX_COLOR + \
+        SUPPORT_XY_COLOR
