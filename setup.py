82836fe504e6209dda064a46dd2fa70b5312193f
Patrik <slasktratt@gammelgravlingen.com>
2017-11-10 22:38:57 +0100

    Rebase setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 
-VERSION = "5.0"
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+VERSION = "5.0.0"
 DOWNLOAD_URL = \
     'https://github.com/ggravlingen/pytradfri/archive/{}.zip'.format(VERSION)
 
 EXTRAS_REQUIRE = {
-    'async': ['aiocoap >= 0.3', 'DTLSSocket >= 0.1.4']
+    'async': ['aiocoap >= 0.4a1', 'DTLSSocket >= 0.1.4']
 }
-
-DEP_LINKS = [
-    "https://github.com/chrysn/aiocoap/archive/3286f48f0b949901c8b5c04c0719dc54ab63d431.zip#egg=aiocoap-0.3"
-]
 
 PACKAGES = find_packages(exclude=['tests', 'tests.*'])
 
@@ -21,10 +30,12 @@ setup(
   version=VERSION,
   description='IKEA Trådfri/Tradfri API. Control and observe your '
               'lights from Python.',
+  long_description=long_description,
   author='balloob, lwis, ggravlingen',
   author_email='no@email.com',
   url='https://github.com/ggravlingen/pytradfri',
+  license='MIT',
+  keywords='ikea tradfri api iot light homeautomation',
   download_url=DOWNLOAD_URL,
   extras_require=EXTRAS_REQUIRE,
-  dependency_links=DEP_LINKS,
 )
