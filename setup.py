ec6d395978983ade665ac8fb0d6af70eedc1598f
Patrik <slasktratt@gammelgravlingen.com>
2017-11-10 21:30:02 +0100

    Add encoding to setup.py and make some amendments (#108)

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 VERSION = "4.0.3"
 DOWNLOAD_URL = \
@@ -17,9 +30,12 @@ setup(
   version=VERSION,
   description='IKEA Trådfri/Tradfri API. Control and observe your '
               'lights from Python.',
+  long_description=long_description,
   author='balloob, lwis, ggravlingen',
   author_email='no@email.com',
   url='https://github.com/ggravlingen/pytradfri',
+  license='MIT',
+  keywords='ikea tradfri api iot light homeautomation',
   download_url=DOWNLOAD_URL,
   extras_require=EXTRAS_REQUIRE,
 )
