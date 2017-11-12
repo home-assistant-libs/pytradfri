602e8ea25e09ac20357b78c189b1d5c501e64dc5
Patrik <slasktratt@gammelgravlingen.com>
2017-11-12 16:29:51 +0100

    Wrap-up

--- example_color.py
+++ example_color.py
@@ -2,11 +2,12 @@
 """
 This is a file to give examples on how to work with colors
 The gateway supports _some_ hex values, otherwise colors stored as XY
+A guess is that IKEA uses the CIE XYZ space
 
 You need to install colormath from pypi in order to this example:
 % pip install colormath
 
-Alternatively as 2.2 of colormath hasn't been released to pypi yet
+Alternatively, as 2.2 of colormath hasn't been released to pypi yet
 % pip install git+git://github.com/gtaylor/python-colormath
 
 The gateway returns:
@@ -20,8 +21,8 @@ The gateway returns:
 
 import sys
 
-from ..pytradfripytradfri import Gateway
-from ..pytradfripytradfri.api.libcoap_api import APIFactory
+from pytradfri import Gateway
+from pytradfri.api.libcoap_api import APIFactory
 
 from colormath.color_conversions import convert_color
 from colormath.color_objects import sRGBColor, XYZColor
@@ -53,16 +54,22 @@ def run():
 
     # Convert RGB to XYZ using a D50 illuminant.
     xyz = convert_color(sRGBColor(rgb[0], rgb[1], rgb[2]), XYZColor,
-                        observer='2', target_illuminant='a')
+                        observer='2', target_illuminant='d65')
+    xy = int(xyz.xyz_x), int(xyz.xyz_y)
 
     #  Assuming lights[3] is a RGB bulb
-    api(lights[3].light_control.set_xy_color(int(xyz.xyz_x/10),
-                                             int(xyz.xyz_y/10)))
+    api(lights[3].light_control.set_xy_color(xy[0], xy[1]))
 
     #  Assuming lights[3] is a RGB bulb
     xy = lights[3].light_control.lights[0].xy_color
-    print(xy)
 
+    #  Normalize Z
+    Z = int(lights[3].light_control.lights[0].dimmer/254*65535)
+    xyZ = xy+(Z,)
+    rgb = convert_color(XYZColor(xyZ[0], xyZ[1], xyZ[2]), sRGBColor,
+                        observer='2', target_illuminant='d65')
+    rgb = (int(rgb.rgb_r), int(rgb.rgb_g), int(rgb.rgb_b))
+    print(rgb)
 
 
 run()
