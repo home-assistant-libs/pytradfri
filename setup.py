19e215794154bb37952081c30806d1e9764b404c
Patrik <slasktratt@gammelgravlingen.com>
2017-11-14 20:00:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = "4.0.4"
+VERSION = "4.0.5"
 DOWNLOAD_URL = \
     'https://github.com/ggravlingen/pytradfri/archive/{}.zip'.format(VERSION)
 
