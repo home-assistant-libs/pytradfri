356295eb07fff12de3337e550a355bd1d59504fe
kjlisby.dk <kj@lisby.dk>
2019-07-17 21:52:09 +0200

    One more time.....

--- tests/test_group.py
+++ tests/test_group.py
@@ -13,7 +13,7 @@ def group():
 def test_setters():
     cmd = Group('anygateway', GROUP) \
         .set_predefined_color('Candlelight')
-    assert cmd.data == "['15004', 131073]: {'5706': 'ebb63e'}"
+    assert cmd.data == {'5706': 'ebb63e'}
 
     with pytest.raises(error.ColorError):
         Group('anygateway', GROUP) \
@@ -21,5 +21,5 @@ def test_setters():
 
     cmd = Group('anygateway', GROUP) \
         .set_hex_color('c984bb')
-    assert cmd.data == "['15004', 131073]: {'5706': 'c984bb'}"
+    assert cmd.data == {'5706': 'c984bb'}
 
