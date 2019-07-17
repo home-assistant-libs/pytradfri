7256f280ef4cd529edc656163c007f5829516638
kjlisby.dk <kj@lisby.dk>
2019-07-17 21:43:53 +0200

    Take three for testing....

--- tests/test_group.py
+++ tests/test_group.py
@@ -11,7 +11,7 @@ def group():
     return Group(GROUP)
 
 def test_setters():
-    cmd = Group(anygateway, GROUP) \
+    cmd = Group('anygateway', GROUP) \
         .set_predefined_color('Candlelight')
     assert cmd.data == {['15004', 131073]: {'5706': 'ebb63e'}}
 
