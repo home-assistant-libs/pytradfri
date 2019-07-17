55ee92d50b8f954a64070372c66bc8bc6729aba1
kjlisby.dk <kj@lisby.dk>
2019-07-17 21:32:43 +0200

    Attemt to add tests for groups. This is "in the blind", since I cannot seem to be able to getting tests to run locally.

---
+++ tests/test_group.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import pytest
+
+from pytradfri import error
+from pytradfri.group import Group
+from devices import (GROUP)
+
+
+@pytest.fixture
+def group():
+    return Group(GROUP)
+
+def test_setters():
+    cmd = Group(GROUP) \
+        .set_predefined_color('Candlelight')
+    assert cmd.data == {['15004', 131073]: {'5706': 'ebb63e'}}
+
+    with pytest.raises(error.ColorError):
+        Group(GROUP) \
+            .set_predefined_color('kjlisby')
+
+    cmd = Group(GROUP) \
+        .set_hex_color('c984bb')
+    assert cmd.data == {['15004', 131073]: {'5706': 'c984bb'}}
+
