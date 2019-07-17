01f59469b230b92eae261d2ace206eab09289978
kjlisby.dk <kj@lisby.dk>
2019-07-17 21:48:32 +0200

    I guess this must be the last part of this goose chase.

--- tests/test_group.py
+++ tests/test_group.py
@@ -13,13 +13,13 @@ def group():
 def test_setters():
     cmd = Group('anygateway', GROUP) \
         .set_predefined_color('Candlelight')
-    assert cmd.data == {['15004', 131073]: {'5706': 'ebb63e'}}
+    assert cmd.data == "['15004', 131073]: {'5706': 'ebb63e'}"
 
     with pytest.raises(error.ColorError):
-        Group(GROUP) \
+        Group('anygateway', GROUP) \
             .set_predefined_color('kjlisby')
 
-    cmd = Group(GROUP) \
+    cmd = Group('anygateway', GROUP) \
         .set_hex_color('c984bb')
-    assert cmd.data == {['15004', 131073]: {'5706': 'c984bb'}}
+    assert cmd.data == "['15004', 131073]: {'5706': 'c984bb'}"
 
