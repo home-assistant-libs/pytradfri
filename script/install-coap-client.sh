04db780d5189ea75756700cb8a3ab39972a5382b
martijn <martijnvwezel@muino.nl>
2020-05-19 17:25:05 +0200

    Update install-coap-client.sh
    Fix missing lib issue and asking for the correct premissions.

--- script/install-coap-client.sh
+++ script/install-coap-client.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run the script as: sudo ./install-coap-client.sh"
+  exit
+fi
+
+apt-get install -f autoconf automake libtool  
+
 git clone --depth 1 --recursive -b dtls https://github.com/home-assistant/libcoap.git
 cd libcoap
 ./autogen.sh
