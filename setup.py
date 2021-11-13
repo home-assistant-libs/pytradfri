9b8ba9fe40b3b536565d85325bd91760b8b2ad4a
Martin Hjelmare <marhje52@gmail.com>
2021-11-13 17:02:25 +0100

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -34,4 +34,15 @@ setup(
     keywords="ikea tradfri api iot light homeautomation",
     download_url=DOWNLOAD_URL,
     extras_require=EXTRAS_REQUIRE,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Home Automation",
+    ],
 )
