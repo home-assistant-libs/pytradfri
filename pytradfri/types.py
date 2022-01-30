50968962ed6dac48607f6f2a0362b7a97a025436
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2022-01-30 13:18:17 +0000

    Fix black error

--- pytradfri/types.py
+++ pytradfri/types.py
@@ -4,9 +4,9 @@ from typing import TypedDict
 TypeDeviceInfo = TypedDict(
     # Alternative syntax required due to the need of using strings as keys:
     # https://www.python.org/dev/peps/pep-0589/#alternative-syntax
-    'TypeDeviceInfo',
+    "TypeDeviceInfo",
     {
-        '0': str,  # Manufacturer
+        "0": str,  # Manufacturer
         "1": str,  # Model number
         "2": str,  # Serial number
         "3": str,  # Firmware version
