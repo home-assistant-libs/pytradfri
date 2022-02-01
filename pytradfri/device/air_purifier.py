c70cb8b44e4dd51a63297b728e7dee05a118b064
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2022-02-01 17:34:52 +0000

    Add more attribues

--- pytradfri/device/air_purifier.py
+++ pytradfri/device/air_purifier.py
@@ -1,22 +1,34 @@
 """Represent an air purifier."""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING, TypedDict, cast
 
-from ..const import (
-    ATTR_AIR_PURIFIER_AIR_QUALITY,
-    ATTR_AIR_PURIFIER_CONTROLS_LOCKED,
-    ATTR_AIR_PURIFIER_FAN_SPEED,
-    ATTR_AIR_PURIFIER_LEDS_OFF,
-    ATTR_AIR_PURIFIER_MODE,
-    ATTR_AIR_PURIFIER_MODE_AUTO,
-    ROOT_AIR_PURIFIER,
-)
-from ..resource import TypeRaw, TypeRawList
+from ..const import ATTR_AIR_PURIFIER_MODE_AUTO, ROOT_AIR_PURIFIER
+from ..resource import TypeRawList
 
 if TYPE_CHECKING:
     # avoid cyclic import at runtime.
     from . import Device
+
+
+TypeAirPurifier = TypedDict(
+    # Alternative syntax required due to the need of using strings as keys:
+    # https://www.python.org/dev/peps/pep-0589/#alternative-syntax
+    "TypeAirPurifier",
+    {
+        "5900": int,  # Mode
+        "5902": int,  # Filter runtume
+        "5903": int,  # Filter status
+        "5904": int,  # Filter lifetime total
+        "5905": int,  # Manual controls locked
+        "5906": int,  # Led light on/off
+        "5907": int,  # Air quality level
+        "5908": int,  # Fan speed
+        "5909": int,  # Motor runtime total
+        "5910": int,  # Filter lifetime remaining
+        "9003": int,  # ID
+    },
+)
 
 
 class AirPurifier:
@@ -28,10 +40,10 @@ class AirPurifier:
         self.index = index
 
     @property
-    def raw(self) -> TypeRaw:
+    def raw(self) -> TypeAirPurifier:
         """Return raw data that it represents."""
         return cast(
-            TypeRaw,
+            TypeAirPurifier,
             cast(TypeRawList, self.device.raw)[ROOT_AIR_PURIFIER][self.index],
         )
 
@@ -42,12 +54,12 @@ class AirPurifier:
 
         Auto mode sets the fan speed automatically based on the air quality.
         """
-        return self.raw[ATTR_AIR_PURIFIER_MODE] == ATTR_AIR_PURIFIER_MODE_AUTO
+        return self.raw["5900"] == ATTR_AIR_PURIFIER_MODE_AUTO
 
     @property
     def state(self) -> bool:
         """Return device state, ie on or off."""
-        return cast(int, self.raw[ATTR_AIR_PURIFIER_MODE]) > 0
+        return self.raw["5900"] > 0
 
     @property
     def fan_speed(self) -> int:
@@ -56,17 +68,17 @@ class AirPurifier:
         0: Device is off
         2..50: Fan speed with a step size of 1.
         """
-        return cast(int, self.raw[ATTR_AIR_PURIFIER_FAN_SPEED])
+        return self.raw["5908"]
 
     @property
     def controls_locked(self) -> bool:
         """Return True if physical controls on the air purifier are locked."""
-        return self.raw[ATTR_AIR_PURIFIER_CONTROLS_LOCKED] == 1
+        return self.raw["5905"] == 1
 
     @property
     def leds_off(self) -> bool:
         """Return True if led's on the air purifier are turned off."""
-        return self.raw[ATTR_AIR_PURIFIER_LEDS_OFF] == 1
+        return self.raw["5906"] == 1
 
     @property
     def air_quality(self) -> int:
@@ -77,4 +89,4 @@ class AirPurifier:
         86..: Not good
         65535: If the fan is off or during measuring time after turning on
         """
-        return cast(int, self.raw[ATTR_AIR_PURIFIER_AIR_QUALITY])
+        return self.raw["5907"]
