bf33c7e4b3bc2fc8a9c3a78cd46103fac14b7d7b
Martin Hjelmare <marhje52@gmail.com>
2022-02-26 21:35:51 +0100

    Use Mapping

--- pytradfri/device/light_control.py
+++ pytradfri/device/light_control.py
@@ -1,6 +1,7 @@
 """Class to control the lights."""
 from __future__ import annotations
 
+from collections.abc import Mapping
 from typing import TYPE_CHECKING
 
 from ..color import COLORS
@@ -98,7 +99,7 @@ class LightControl(BaseController):
         """
         self._value_validate(dimmer, RANGE_BRIGHTNESS, "Dimmer")
 
-        values: dict[str, str | int] = {ATTR_LIGHT_DIMMER: dimmer}
+        values = {ATTR_LIGHT_DIMMER: dimmer}
 
         if transition_time is not None:
             values[ATTR_TRANSITION_TIME] = transition_time
@@ -111,7 +112,7 @@ class LightControl(BaseController):
         """Set color temp a light."""
         self._value_validate(color_temp, RANGE_MIREDS, "Color temperature")
 
-        values: dict[str, str | int] = {ATTR_LIGHT_MIREDS: color_temp}
+        values = {ATTR_LIGHT_MIREDS: color_temp}
 
         if transition_time is not None:
             values[ATTR_TRANSITION_TIME] = transition_time
@@ -143,7 +144,7 @@ class LightControl(BaseController):
         self._value_validate(color_x, RANGE_X, "X color")
         self._value_validate(color_y, RANGE_Y, "Y color")
 
-        values: dict[str, str | int] = {
+        values = {
             ATTR_LIGHT_COLOR_X: color_x,
             ATTR_LIGHT_COLOR_Y: color_y,
         }
@@ -166,7 +167,7 @@ class LightControl(BaseController):
         self._value_validate(hue, RANGE_HUE, "Hue")
         self._value_validate(saturation, RANGE_SATURATION, "Saturation")
 
-        values: dict[str, str | int] = {
+        values = {
             ATTR_LIGHT_COLOR_SATURATION: saturation,
             ATTR_LIGHT_COLOR_HUE: hue,
         }
@@ -192,7 +193,7 @@ class LightControl(BaseController):
         except KeyError:
             raise ColorError(f"Invalid color specified: {colorname}") from KeyError
 
-    def set_values(self, values: dict[str, str | int], *, index: int = 0) -> Command:
+    def set_values(self, values: Mapping[str, str | int], *, index: int = 0) -> Command:
         """Set values on light control.
 
         Returns a Command.
