f3a952ce0cb5fd4113ecbf680582ff67133b3721
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2022-04-22 17:08:21 +0000

    Clarify meaning of filter_status property

--- pytradfri/device/air_purifier.py
+++ pytradfri/device/air_purifier.py
@@ -92,9 +92,9 @@ class AirPurifier:
         return self.raw.filter_runtime
 
     @property
-    def filter_status(self) -> int:
-        """Return filter status."""
-        return self.raw.filter_status
+    def filter_status(self) -> bool:
+        """Return true if filter needs to be replaced."""
+        return bool(self.raw.filter_status)
 
     @property
     def is_auto_mode(self) -> bool:
