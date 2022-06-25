3b59f902a3e9acabc0ad0b9617b3bb813f921252
Martin Hjelmare <marhje52@gmail.com>
2022-06-25 23:12:23 +0200

    Fix test_command

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,13 +1,11 @@
 """Test Command."""
-import pytest
-
 from pytradfri.command import Command
 
 
 def test_property_access():
     """Test property access in Command."""
 
-    def ec():
+    def error_callback():
         pass
 
     command = Command(
@@ -17,7 +15,7 @@ def test_property_access():
         parse_json=True,
         observe=False,
         observe_duration=0,
-        err_callback=ec,
+        err_callback=error_callback,
     )
 
     assert command.method == "method"
@@ -25,16 +23,16 @@ def test_property_access():
     assert command.parse_json is True
     assert command.observe is False
     assert command.observe_duration == 0
-    assert command.err_callback == ec
+    assert command.err_callback is error_callback
 
 
 def test_result():
     """Test callback process_result."""
 
-    def pr(value):
+    def process_result(value):
         return value + 1
 
-    command = Command("method", "path", {}, process_result=pr)
+    command = Command("method", "path", {}, process_result=process_result)
     assert command.result is None
     assert command.raw_result is None
 
@@ -52,11 +50,3 @@ def test_url():
     command2 = Command("method", ["path1", "path2"], {})
     url = command2.url("host")
     assert url == "coaps://host:5684/path1/path2"
-
-
-def test_add_unsupported():
-    """Test add unsupported causes error."""
-    command1 = Command("method", "path", {})
-    not_a_command = 0
-    with pytest.raises(TypeError):
-        command1 + not_a_command
