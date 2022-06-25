3b23f29221662c263223a649c989a89753396f36
Martin Hjelmare <marhje52@gmail.com>
2022-06-25 23:40:28 +0200

    Fix test_util

--- tests/test_util.py
+++ tests/test_util.py
@@ -17,11 +17,11 @@ class UtilTestsBitChoices(unittest.TestCase):
 
     def test_bitchoices(self):
         """Test bit choices."""
-        WEEKDAYS = BitChoices((("tue", "Tuesday"),))
+        weekdays = BitChoices((("tue", "Tuesday"),))
 
-        assert WEEKDAYS.get_selected_keys(1) == ["tue"]
-        assert len(WEEKDAYS) == 1
-        assert [x for x in WEEKDAYS] == [(1, "Tuesday")]  # noqa: C416
+        assert weekdays.get_selected_keys(1) == ["tue"]
+        assert len(weekdays) == 1
+        assert list(weekdays) == [(1, "Tuesday")]
 
 
 class UtilTestsJSON(unittest.TestCase):
@@ -37,19 +37,20 @@ class UtilTestsJSON(unittest.TestCase):
 
     def test_json_save(self):
         """Test json save."""
-        FILENAME = path.join(self.test_dir, "sample_psk_file.txt")
+        filename = path.join(self.test_dir, "sample_psk_file.txt")
         conf = {"identity": "pytradfri", "key": "123abc"}
 
-        written_file = save_json(FILENAME, conf)
+        written_file = save_json(filename, conf)
         self.assertTrue(written_file)
 
     def test_json_load(self):
         """Test json load."""
-        f = open(path.join(self.test_dir, "sample_psk_file2.txt"), "w")
         config = {"identity": "hashstring", "key": "secretkey"}
         data = json.dumps(config, sort_keys=True, indent=4)
-        f.write(data)
-        f.close()
+        with open(
+            path.join(self.test_dir, "sample_psk_file2.txt"), "w", encoding="utf-8"
+        ) as fil:
+            fil.write(data)
 
         json_data = load_json(path.join(self.test_dir, "sample_psk_file2.txt"))
         self.assertEqual(json_data, {"identity": "hashstring", "key": "secretkey"})
@@ -60,20 +61,21 @@ class UtilTestsJSON(unittest.TestCase):
 
     def test_load_not_json(self):
         """Test of load not json."""
-        f = open(path.join(self.test_dir, "sample_psk_file3.txt"), "w")
         data = "{not valid json"
-        f.write(data)
-        f.close()
+        with open(
+            path.join(self.test_dir, "sample_psk_file3.txt"), "w", encoding="utf-8"
+        ) as fil:
+            fil.write(data)
 
         with pytest.raises(PytradfriError):
             load_json(path.join(self.test_dir, "sample_psk_file3.txt"))
 
     def test_save_not_serializable(self):
         """Test save of illegal path."""
-        FILENAME = path.join(self.test_dir, "should_not_save")
+        filename = path.join(self.test_dir, "should_not_save")
         conf = b"bytes are not serializable"
         with pytest.raises(PytradfriError):
-            save_json(FILENAME, conf)
+            save_json(filename, conf)
 
     def test_os_error(self):
         """Test os error is thrown."""
