19e880abd2412be63d89e30e8ad334c434d49aec
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2023-03-05 18:59:22 +0000

    Add ignores for pylint and flake8

--- examples/example_sync.py
+++ examples/example_sync.py
@@ -23,7 +23,7 @@ import uuid
 folder = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.normpath(f"{folder}/.."))
 
-# pylint: disable=import-error, wrong-import-position, useless-suppression
+# pylint: disable=import-error, wrong-import-position, useless-suppression, not-an-iterable, unsubscriptable-object # noqa: E501
 
 from pytradfri import Gateway
 from pytradfri.api.libcoap_api import APIFactory, APIRequestProtocol
