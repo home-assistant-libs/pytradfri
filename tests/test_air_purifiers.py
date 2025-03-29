e2f8736fc7bdee3e5004c215e536abcc8ccdc64a
Martin Hjelmare <marhje52@gmail.com>
2025-03-29 17:39:24 +0100

    Move air purifier fixture and complete air purifier tests (#1013)

--- tests/test_air_purifiers.py
+++ tests/test_air_purifiers.py
@@ -1,16 +1,28 @@
 """Test Air Purifiers."""
 
+import json
+from typing import Any
+
 import pytest
 
 from pytradfri.device import Device
 
-from .devices import AIR_PURIFIER
+from tests.common import load_fixture
+
+pytestmark = pytest.mark.parametrize("device", ["air_purifier"], indirect=True)
 
 
 @pytest.fixture(name="device")
-def device_fixture() -> Device:
-    """Return Device."""
-    return Device(AIR_PURIFIER)
+def device_fixture(request: pytest.FixtureRequest) -> Device:
+    """Return a device."""
+    device_response: dict[str, Any] = json.loads(request.getfixturevalue(request.param))
+    return Device(device_response)
+
+
+@pytest.fixture(name="air_purifier")
+def air_purifier_fixture() -> str:
+    """Return a air_purifier response."""
+    return load_fixture("air_purifier.json")
 
 
 def test_device_info_properties(device: Device) -> None:
@@ -87,3 +99,45 @@ def test_filter_runtime(device: Device) -> None:
     """Test filter's run time."""
     air_purifier = device.air_purifier_control.air_purifiers[0]
     assert air_purifier.filter_runtime == 2
+
+
+def test_turn_off(device: Device) -> None:
+    """Test turn off."""
+    air_purifier_control = device.air_purifier_control
+    assert air_purifier_control is not None
+    command = air_purifier_control.turn_off()
+    assert command.data == {"15025": [{"5900": 0}]}
+
+
+def test_turn_on_auto_mode(device: Device) -> None:
+    """Test turn on auto mode."""
+    air_purifier_control = device.air_purifier_control
+    assert air_purifier_control is not None
+    command = air_purifier_control.turn_on_auto_mode()
+    assert command.data == {"15025": [{"5900": 1}]}
+
+
+def test_set_fan_speed(device: Device) -> None:
+    """Test set fan speed."""
+    air_purifier_control = device.air_purifier_control
+    assert air_purifier_control is not None
+    command = air_purifier_control.set_fan_speed(10)
+    assert command.data == {"15025": [{"5900": 10}]}
+
+
+@pytest.mark.parametrize(("locked", "expected_value"), [(True, 1), (False, 0)])
+def test_set_controls_locked(device: Device, locked: bool, expected_value: int) -> None:
+    """Test set controls locked."""
+    air_purifier_control = device.air_purifier_control
+    assert air_purifier_control is not None
+    command = air_purifier_control.set_controls_locked(locked)
+    assert command.data == {"15025": [{"5905": expected_value}]}
+
+
+@pytest.mark.parametrize(("leds_off", "expected_value"), [(True, 1), (False, 0)])
+def test_set_leds_off(device: Device, leds_off: bool, expected_value: int) -> None:
+    """Test set leds off."""
+    air_purifier_control = device.air_purifier_control
+    assert air_purifier_control is not None
+    command = air_purifier_control.set_leds_off(leds_off)
+    assert command.data == {"15025": [{"5906": expected_value}]}
