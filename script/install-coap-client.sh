be70e765f336944225552fbe17431c710cff9edc
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2025-12-25 12:56:41 +0100

    Update libcoap repository URL in install script

--- script/install-coap-client.sh
+++ script/install-coap-client.sh
@@ -7,7 +7,7 @@ fi
 
 apt-get install -f autoconf automake libtool  
 
-git clone --depth 1 --recursive -b dtls https://github.com/home-assistant/libcoap.git
+git clone --depth 1 --recursive -b dtls https://github.com/obgm/libcoap.git
 cd libcoap
 ./autogen.sh
 ./configure --disable-documentation --disable-shared --without-debug CFLAGS="-D COAP_DEBUG_FD=stderr"
